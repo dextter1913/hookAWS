0c72c7ecb2651efab320364a014f3c414d34e837
dextter1913 <dextter1913@gmail.com>
2022-06-15 16:47:39 -0500

    test

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ header("Content-Type: JSON");
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
         $_POST = json_decode(file_get_contents('php://input'), true);
-        shell_exec("cd /var/www/html/stefy && sudo git pull origin master");
+        shell_exec("cd /var/www/html/ && git clone https://github.com/dextter1913/stefy.git");
+        // shell_exec("cd /var/www/html/stefy && sudo git pull origin master");
         shell_exec("touch log.txt");
         file_put_contents('log.txt', json_encode($_POST));
         echo json_encode($_POST);
