42d4640e0a4d17c6ea686b735a1976a6730413d5
dextter1913 <dextter1913@gmail.com>
2022-06-15 14:41:06 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<?php
+header("Content-Type: JSON");
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+        $_POST = json_decode(file_get_contents('php://input'), true);
+        echo json_encode($_POST);
+
+        break;
+    case 'PUT':
+        $_PUT = json_decode(file_get_contents('php://input'), true);
+
+        break;
+    case 'DELETE':
+        $_DELETE = json_decode(file_get_contents('php://input'), true);
+
+        break;
+    case 'GET':
+        header("Content-Type: JSON");
+        $_GET = json_decode(file_get_contents('php://input'), true);
+
+        break;
+    default:
+        echo "Metodo no permitido";
+        http_response_code(405);
+}
