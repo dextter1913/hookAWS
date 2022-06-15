80bfff40ac14ba1ac4568a527d38f4f5cc1ea0fa
dextter1913 <dextter1913@gmail.com>
2022-06-15 15:39:20 -0500

    probando hook

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ header("Content-Type: JSON");
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
         $_POST = json_decode(file_get_contents('php://input'), true);
+        file_put_contents('log.txt', json_encode($_POST));
         echo json_encode($_POST);
 
         break;
